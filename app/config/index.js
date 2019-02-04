025c9c34e80094697d07eb00836fd9d3afc2802f
dwhieb <dwhieb@gmail.com>
2019-02-04 16:49:11 -0600

    document new config variables

--- app/config/index.js
+++ app/config/index.js
@@ -11,11 +11,13 @@ Object.assign(process.env, settings);
 /**
  * A hash of environment variables for the app
  * @type {Object}
- * @prop {String}  env         - The current environment. `localhost` || `production`
- * @prop {Boolean} localhost   - Whether the app is running on localhost
- * @prop {Boolean} logRequests - Whether to log details about incoming requests
- * @prop {Integer} port        - The port that the app is running on
- * @prop {Boolean} production  - Whether the app is running on production (including the `staging` slot)
+ * @prop {String}  env           - The current environment. `localhost` || `production`
+ * @prop {Boolean} localhost     - Whether the app is running on localhost
+ * @prop {Boolean} logAppErrors  - Whether the app should log 5xx errors to the console
+ * @prop {Boolean} logRequests   - Whether to log details about incoming requests
+ * @prop {Boolean} logUserErrors - Whether the app should log 4xx errors to the console
+ * @prop {Integer} port          - The port that the app is running on
+ * @prop {Boolean} production    - Whether the app is running on production (including the `staging` slot)
  */
 module.exports = {
   env:         process.env.NODE_ENV,
