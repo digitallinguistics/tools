e3acc719541437a8a1932e81af692dc8e5dc606d
Daniel W. Hieber <dwhieb@gmail.com>
2019-02-04 20:29:48 -0600

    CODE: set Vary header on incoming requests (#84)
    closes #84

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -6,10 +6,12 @@ const errors = require(`./errors`);
 const helmet = require(`./helmet`);
 const logger = require(`./logger`);
 const serve  = require(`./static`);
+const vary   = require(`./vary`);
 
 module.exports = {
   errors,
   helmet,
   logger,
   serve,
+  vary,
 };
